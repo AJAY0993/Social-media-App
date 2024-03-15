0cdb38719ad28788a81903bb5a2d7a6d657d07d6
AJAY0993 <ajaymishra5277919886@gmail.com>
2024-03-15 17:24:42 +0530

    Added production url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL:
-    import.meta.env === "production" ? "/api" : "http://localhost:3000/api/",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:3000/api/"
+    : "https://social-media-app-kxxc.onrender.com/api/",
   withCredentials: true
 })
